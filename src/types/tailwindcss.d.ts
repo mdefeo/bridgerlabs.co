a2b4430ead4dc557fd0adc85f67e3843a7695706
Marcello <mdefeo@users.noreply.github.com>
2025-02-15 01:21:29 -0500

    Clean install, import of data

--- src/types/tailwindcss.d.ts
+++
@@ -1,5 +0,0 @@
-// tailwindcss.d.ts
-declare module "tailwindcss/lib/util/flattenColorPalette" {
-  const flattenColorPalette: (colors: unknown) => Record<string, string>;
-  export default flattenColorPalette;
-}
---
+++ types/tailwindcss.d.ts
@@ -0,0 +1,4 @@
+// tailwindcss.d.ts
+declare module 'tailwindcss/lib/util/flattenColorPalette' {
+  export default function flattenColorPalette(colors: any): Record<string, string>;
+}
